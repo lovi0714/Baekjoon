2556cba7aadff30503288dc7ed477b4f9f3c3b90
lovi0714 <95499211+lovi0714@users.noreply.github.com>
2021-12-16 21:07:42 +0900

    아스키 코드
    1. System.in.read() -> 시스템이 가지고 있는 입력 장치에서 입력된 키코드를 읽어라.
    2. main() 메소드 끝에 throws Exception은  System.in.read() 를 사용할 때에 대한 예외 처리 코드이다. 단순히 모니터에 예외 내용을 출력만 한다.
    3. throws Exception 더 찾아보기.

---
+++ 11654번.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main{
+	public static void main(String []args) throws IOException {
+		int keycode = System.in.read();
+		System.out.println(keycode);
+		}
+}

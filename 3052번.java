01e06615cb2f8eabd4f46cc0b14b96f69fff7f16
이호인 <95499211+lovi0714@users.noreply.github.com>
2022-05-11 08:57:13 +0900

    나머지
    코드 변경~

--- 3052번.java
+++ 3052번.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        HashSet<Integer> result = new HashSet<Integer>();
+        System.out.println(getResult());
+    }
+     static <E> int getResult() {
+        HashSet<Integer> result = new HashSet<>();
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
+        for(int i = 0; i < 10; i++){
             int number = sc.nextInt();
-            int remainNumber = getResult(number);
+            int remainNumber = number%42;
             result.add(remainNumber);
         }
-        System.out.println(result.size());
-    }
-     static int getResult(int number) {
-         return number % 42;
+        return result.size();
     }
 }
